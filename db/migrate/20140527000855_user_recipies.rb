b27775eba9b089fdf7b868ebc51c3d92c3a1cf84
Tyler Goff <tgoff@lsoc.org>
2014-05-29 13:38:40 +0000

    edits migration

--- db/migrate/20140527000855_user_recipies.rb
+++ db/migrate/20140527000855_user_recipies.rb
@@ -1,4 +1,10 @@
 class UserRecipies < ActiveRecord::Migration
   def change
+    create_table :user_recipes do |t|
+      t.integer :user_id
+      t.integer :recipe_id
+
+      t.timestamps
+    end
   end
 end
